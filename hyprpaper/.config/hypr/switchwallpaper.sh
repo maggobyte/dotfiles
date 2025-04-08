5e1d553e211fbf107e8793f049c0775333b696f1
maggobyte <magnust97@gmail.com>
2025-04-08 16:41:19 +0200

    Comments on hyprpaper script

--- hyprpaper/.config/hypr/switchwallpaper.sh
+++ hyprpaper/.config/hypr/switchwallpaper.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env bash
 
 WALLPAPER_DIR="$HOME/Pictures/Wallpaper"
@@ -12,7 +11,7 @@ CURRENT_WALL=$(hyprctl hyprpaper listloaded | grep -oP '(?<=path: ).*' | head -n
 # Get a random wallpaper that's not the current one
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
-# Unload all current wallpapers
+# Unload all current wallpapers - Causes flicker, but saves RAM
 hyprctl hyprpaper unload all
 
 # Preload and apply the same wallpaper to all monitors
@@ -20,4 +19,3 @@ hyprctl hyprpaper preload "$WALLPAPER"
 for MON in "${MONITORS[@]}"; do
     hyprctl hyprpaper wallpaper "$MON,$WALLPAPER"
 done
-
